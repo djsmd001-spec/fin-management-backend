abdeea33980a2b7e0fc246874b7ba5d762e1e22d
gorakh <djsmd001@gmail.com>
2026-02-15 15:39:24 +0530

    Fixed Gmail SMTP timeout issue

--- config/email.js
+++ config/email.js
@@ -1,12 +1,15 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
+  secure: true,
+  port: 465, // 🔥 important
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
