89ba645c1fb318ae3dabcc279c67528fd26e6e79
gorakh <djsmd001@gmail.com>
2026-02-15 16:06:17 +0530

    Force IPv4 fix for Gmail Railway

--- config/email.js
+++ config/email.js
@@ -1,21 +1,20 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+// 🔥 Force IPv4
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    ciphers: "SSLv3",
-    rejectUnauthorized: false,
-  },
-  connectionTimeout: 10000,  // 10 sec
+  connectionTimeout: 15000,
   greetingTimeout: 10000,
-  socketTimeout: 15000,
+  socketTimeout: 20000,
 });
 
 module.exports = transporter;
