67b9944ceeb77481ddb08a2699642cf115540c74
gorakh <djsmd001@gmail.com>
2026-02-15 15:58:41 +0530

    Force TLS fix for Gmail on Railway

--- config/email.js
+++ config/email.js
@@ -1,16 +1,21 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  secure: true,
-  port: 465, // 🔥 important
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
   tls: {
+    ciphers: "SSLv3",
     rejectUnauthorized: false,
   },
+  connectionTimeout: 10000,  // 10 sec
+  greetingTimeout: 10000,
+  socketTimeout: 15000,
 });
 
 module.exports = transporter;
